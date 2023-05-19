0491106cc9a0fe92bfaf14bea9c2ec338b717703
NVadimD <vadim.zelep@gmail.com>
2023-05-19 10:55:08 +0700

    Дописал LS, задачник работает

--- js/main.js
+++ js/main.js
@@ -5,6 +5,13 @@ const emptyList = document.getElementById('emptyList');
 
 let tasks = [];
 
+if (localStorage.getItem('tasks')) {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+    rendorTasks();
+}
+
+
+
 function addTask(event) {
     event.preventDefault();
     const taskText = taskInput.value;
@@ -17,6 +24,7 @@ function addTask(event) {
         done: false
     }
     tasks.push(newTask);
+    saveToLocalStorage();
 
     const cssClass = newTask.done ? 'task-title task-title--done' : 'task-title';
    
@@ -34,7 +42,7 @@ function addTask(event) {
     </div>
     </li>`);
     taskInput.value = '';
-    if (tasksList.children.length > 1) {
+    if (tasks.length > 0) {
         emptyList.classList.add('none');
     }
 }
@@ -50,9 +58,10 @@ function deleteTask(event) {
             }
         })
         tasks.splice(index, 1);
+        saveToLocalStorage();
 
         parentNode.remove();
-        if (tasksList.children.length === 1) {
+        if (tasks.length === 0) {
             emptyList.classList.remove('none');
         }
     }
@@ -63,12 +72,9 @@ function doneTask(event) {
         const parentNode = event.target.closest('li');
 
         const id = parentNode.id;
-        const task = tasks.find((task) => {
-            if (task.id == id) {
-                return true;
-            }
-        })
+        const task = tasks.find(task => task.id == id) 
         task.done = !task.done;
+        saveToLocalStorage();
 
         const taskText = parentNode.querySelector('span');
         taskText.classList.toggle('task-title--done');
@@ -86,3 +92,34 @@ form.addEventListener('submit', addTask);
 tasksList.addEventListener('click', deleteTask);
 
 tasksList.addEventListener('click', doneTask);
+
+
+
+
+
+// ----------------------------------функция записи объекта tasks в LS------------------------------------
+function saveToLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function rendorTasks() {
+    tasks.forEach(function(task) {
+        const cssClass = task.done ? 'task-title task-title--done' : 'task-title';
+    
+        tasksList.insertAdjacentHTML("beforeEnd", `<li id="${task.id}" class="list-group-item d-flex justify-content-between task-item">
+        <span class="${cssClass}">${task.text}</span>
+        <div class="task-item__buttons">
+            <button type="button" data-action="done" class="btn-action">
+                <img src="./img/tick.svg" alt="Done" width="18" height="18">
+            </button>
+            <button type="button" data-action="delete" class="btn-action">
+                <img src="./img/cross.svg" alt="Done" width="18" height="18">
+            </button>
+        </div>
+        </li>`);
+        taskInput.value = '';
+        if (tasks.length > 0) {
+            emptyList.classList.add('none');
+        }
+    })
+}
